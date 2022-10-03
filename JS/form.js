aa8ba4c85ce68e721eba6c207e42f0bfd3661114
Diogenes-Henrique <110642863+Diogenes-Henrique@users.noreply.github.com>
2022-10-03 18:58:30 -0300

    enviando os arquivos
    enviando os arquivos

---
+++ JS/form.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", () =>{
+    console.log("cliquei")
+});
+
+menu.addEventListener("mouseover", () =>{
+    console.log("passei o mouse")
+});
+
+menu.addEventListener("mouseout", () =>{
+    console.log("tirei o mouse")
+});
